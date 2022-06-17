9ae7dcee6bbfa29b4cb7455f060a778648dcdb56
Tanvir Rahman <tanvirhmn@users.noreply.github.com>
2022-06-17 12:28:28 +0600

    First Push

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Required]
+        [Column("CardHolderName",TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string SecurityCode { get; set; }
+
+
+    }
+}
